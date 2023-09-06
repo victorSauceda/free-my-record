8b6fa7125b445443429177cc54452e0f7cdc6f2f
victorSauceda <victorasauceda@gmail.com>
2023-09-06 13:51:36 -0500

    service provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,31 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import { appWithTranslation } from 'next-i18next';
+import Head from 'next/head';
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 
 import { UserProvider } from '../context/UserContext';
-
-function App({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+  session: Session;
+}
+function App({ Component, pageProps, session }: MyAppProps) {
   return (
-    <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </UserProvider>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="-2GddDfhrkTpIBW-wrFLeFF2skPg2g06Mf7ioEaFom4"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <UserProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </UserProvider>
+      </SessionProvider>
+    </>
   );
 }
 export default appWithTranslation(App);
